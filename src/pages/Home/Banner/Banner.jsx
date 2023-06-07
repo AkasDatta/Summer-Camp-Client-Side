5b0a4405957dfb514ece32c8a486ebe4a32cb3d0
Akas_Datta <012akasdatta@gmail.com>
2023-06-08 00:19:46 +0600

    banner component image modified

--- src/pages/Home/Banner/Banner.jsx
+++ src/pages/Home/Banner/Banner.jsx
@@ -1,9 +1,9 @@
 import { useState, useEffect } from 'react';
 import { Container, Col, Button } from 'react-bootstrap';
 import './Banner.css';
-import backgroundImage1 from '../../../assets/1.jpg';
-import backgroundImage2 from '../../../assets/2.jpg';
-import backgroundImage3 from '../../../assets/karim-manjra-cW3nDFVpi10-unsplash.jpg';
+import backgroundImage1 from '../../../assets/8.jpg';
+import backgroundImage2 from '../../../assets/karim-manjra-cW3nDFVpi10-unsplash.jpg';
+import backgroundImage3 from '../../../assets/2.jpg';
 
 const Banner = () => {
   const [currentIndex, setCurrentIndex] = useState(0);
@@ -26,7 +26,7 @@ const Banner = () => {
   useEffect(() => {
     const timer = setInterval(() => {
       setCurrentIndex((prevIndex) => (prevIndex + 1) % backgrounds.length);
-    }, 15000);
+    }, 10000);
 
     return () => clearInterval(timer);
   });
@@ -38,22 +38,36 @@ const Banner = () => {
         style={{
           minHeight: '750px',
           background: `url(${backgrounds[currentIndex]}) no-repeat center top`,
-          backgroundSize: 'cover',
+          backgroundSize: '100% 100%',
         }}
       >
         <div className="dark-overlay">
           <div className="banner-inner">
             <Container>
               <Col md={{ span: 8, offset: 2 }} lg={{ span: 6, offset: 3 }} className="text-center">
-                <h1 data-aos="fade-down" data-aos-easing="linear"
-                data-aos-duration="1500" className="fw-bold display-1" style={{ color: '#fff' }}>
+                <h1
+                  data-aos="fade-down"
+                  data-aos-easing="linear"
+                  data-aos-duration="1500"
+                  className="fw-bold display-1"
+                  style={{ color: '#fff' }}
+                >
                   {texts[currentIndex].heading}
                 </h1>
-                <p data-aos="fade-zoom-in"
-     data-aos-easing="ease-in-back"
-     data-aos-delay="300"
-     data-aos-offset="0"className="text-light">{texts[currentIndex].description}</p>
-                <Button data-aos="fade-up" data-aos-duration="3000" variant="btn btn-outline-light btn-warning text-dark border-danger rounded-5 px-5 py-2">
+                <p
+                  data-aos="fade-zoom-in"
+                  data-aos-easing="ease-in-back"
+                  data-aos-delay="300"
+                  data-aos-offset="0"
+                  className="text-light"
+                >
+                  {texts[currentIndex].description}
+                </p>
+                <Button
+                  data-aos="fade-up"
+                  data-aos-duration="3000"
+                  variant="btn btn-outline-light btn-warning text-dark border-danger rounded-5 px-5 py-2"
+                >
                   Start Learning
                 </Button>
               </Col>
