2c227e08f2a811d55feaaf5385fd70b46ae0550a
Akas_Datta <012akasdatta@gmail.com>
2023-06-13 20:02:29 +0600

    google login information save mongodb

--- src/pages/Shared/SocialLogin/SocialLogin.jsx
+++ src/pages/Shared/SocialLogin/SocialLogin.jsx
@@ -16,7 +16,19 @@ const SocialLogin = () => {
       .then(result => {
         const loggedInUser = result.user;
         console.log(loggedInUser);
-        navigate(from, { replace: true });
+        const mySaveUser = {name: loggedInUser.displayName, email: loggedInUser.email}
+        fetch('http://localhost:5000/savedusers',{
+          method: 'POST',
+          headers: {
+            'content-type':'application/json'
+          },
+          body: JSON.stringify(mySaveUser)
+        })
+        .then(res => res.json())
+        .then(() => {
+            navigate(from, { replace: true });
+        })
+
       })
   }
   
