31d52c14520025254cf023ab95fef7ad955f715e
Akas_Datta <012akasdatta@gmail.com>
2023-06-13 22:53:23 +0600

    added admin to dashboard

--- src/Layout/Dashboard/Dashboard.jsx
+++ src/Layout/Dashboard/Dashboard.jsx
@@ -8,14 +8,16 @@ import {
 } from '@ant-design/icons';
 import './Dashboard.css';
 import { Link, Outlet } from 'react-router-dom';
-import { FaChalkboardTeacher, FaUsers } from 'react-icons/fa';
+import { FaChalkboardTeacher, FaList, FaUsers } from 'react-icons/fa';
 import useCart from '../../hooks/useCart';
 import { RiShoppingBag2Line } from 'react-icons/ri';
+import { MdAddShoppingCart, MdLibraryBooks } from "react-icons/md";
 
 const { Sider, Content } = Layout;
 
 const Dashboard = () => {
   const [cart] = useCart();
+  const isAdmin = true;
   const [collapsed, setCollapsed] = useState(false);
 
   const toggleCollapsed = () => {
@@ -31,33 +33,53 @@ const Dashboard = () => {
       <Sider collapsible collapsed={collapsed} onCollapse={toggleCollapsed}>
         <div className="demo-logo-vertical mt-5" />
         <Menu theme="dark" defaultSelectedKeys={['1']} mode="inline">
-          <Menu.Item key="1" icon={<HomeOutlined />}>
-            <span>User Home</span>
-          </Menu.Item>
-          <Menu.Item key="2" icon={<CalendarOutlined />}>
-            <span>Reservations</span>
-          </Menu.Item>
-          <Menu.Item key="3" icon={<WalletOutlined />}>
-            <span>Payment History</span>
-          </Menu.Item>
-
-          <Menu.Item key="4" icon={<RiShoppingBag2Line />}>
-            <Link as={Link} className='text-decoration-none' to="/dashboard/mycart">
-              My Cart
-              <span
-                className="badge mx-2"
-                style={{
-                  backgroundColor: '#0C4B65',
-                  color: 'white',
-                  borderRadius: '50%',
-                  fontSize: '9px',
-                  textAlign: 'center',
-                }}
-              >
-                +{cart?.length || 0}
-              </span>
-            </Link>
-          </Menu.Item>
+        {
+          isAdmin ? <>
+                <Menu.Item key="1" icon={<HomeOutlined />}>
+                <Link className='text-decoration-none' to="/dashboard/home">Admin Home</Link>
+                </Menu.Item>
+                <Menu.Item key="2" icon={<MdAddShoppingCart />}>
+                <Link className='text-decoration-none' to="/dashboard/reservations">Add Items</Link>
+                </Menu.Item>
+                <Menu.Item key="3" icon={<FaList />}>
+                <Link className='text-decoration-none' to="/dashboard/home">Manage Items</Link>
+                </Menu.Item>
+                <Menu.Item key="4" icon={<MdLibraryBooks />}>
+                <Link className='text-decoration-none' to="/dashboard/home">Manage Bookings</Link>
+                </Menu.Item>
+                <Menu.Item key="5" icon={<FaUsers />}>
+                <Link className='text-decoration-none' to="/dashboard/home">All Users</Link>
+                </Menu.Item>
+          </> : <>
+             <Menu.Item key="1" icon={<HomeOutlined />}>
+                <Link className='text-decoration-none' to="/dashboard/home">User Home</Link>
+                </Menu.Item>
+                <Menu.Item key="2" icon={<CalendarOutlined />}>
+                <Link className='text-decoration-none' to="/dashboard/reservations">Reservations</Link>
+                </Menu.Item>
+                <Menu.Item key="3" icon={<WalletOutlined />}>
+                <Link className='text-decoration-none' to="/dashboard/history">Payment History</Link>
+                </Menu.Item>
+                <Menu.Item key="4" icon={<RiShoppingBag2Line />}>
+                  <Link as={Link} className='text-decoration-none' to="/dashboard/mycart">
+                    My Cart
+                    <span
+                      className="badge mx-2"
+                      style={{
+                        backgroundColor: '#0C4B65',
+                        color: 'white',
+                        borderRadius: '50%',
+                        fontSize: '9px',
+                        textAlign: 'center',
+                      }}
+                    >
+                      +{cart?.length || 0}
+                    </span>
+                  </Link>
+                </Menu.Item>
+          </>
+        }
+         
           <hr className='text-white' />
 
           <Menu.Item key="5" icon={<HomeOutlined />}>
