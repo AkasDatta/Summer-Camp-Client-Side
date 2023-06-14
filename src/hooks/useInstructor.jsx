f6fc69e8e2e661b3a481fc14f82b59fc5776058c
Akas_Datta <012akasdatta@gmail.com>
2023-06-15 03:19:13 +0600

    modified usecart and added useInstructor hooks

--- src/hooks/useInstructor.jsx
+++ src/hooks/useInstructor.jsx
@@ -1,19 +1,46 @@
+import { useContext } from "react";
+import { AuthContext } from "../Providers/AuthProvider/AuthProvider";
 import { useQuery } from "@tanstack/react-query";
-import useAuth from "./useAuth";
-import useAxiosSecure from "./useAxiosSecure";
 
 const useInstructor = () => {
-    const {user, loading} = useAuth();
-    const [axiosSecure] = useAxiosSecure();
-    // use axios secure with react query
-    const {data: isInstructor, isLoading: isInstructorLoading} = useQuery({
-        queryKey: ['isInstructor', user?.email],
-        enabled: !loading,
-        queryFn: async () => {
-            const res = await axiosSecure.get(`/savedusers/instructor/${user?.email}`);
-            return res.data.instructor;
+  const { user } = useContext(AuthContext);
+  const token = localStorage.getItem("access-token");
+  const { data: isInstructor, isLoading: isInstructorLoading } = useQuery({
+    queryKey: ["isInstructor", user?.email],
+    queryFn: async () => {
+      const res = await fetch(
+        `http://localhost:5000/users/instructor/${user?.email}`,
+        {
+          headers: {
+            authorization: `bearer ${token}`,
+          },
         }
-    })
-    return [isInstructor, isInstructorLoading]
-}
+      );
+      return res.json();
+    },
+  });
+  return [isInstructor, isInstructorLoading];
+};
+
 export default useInstructor;
+
+
+// import { useQuery } from "@tanstack/react-query";
+// import useAuth from "./useAuth";
+// import useAxiosSecure from "./useAxiosSecure";
+
+// const useInstructor = () => {
+//     const {user, loading} = useAuth();
+//     const [axiosSecure] = useAxiosSecure();
+//     // use axios secure with react query
+//     const {data: isInstructor, isLoading: isInstructorLoading} = useQuery({
+//         queryKey: ['isInstructor', user?.email],
+//         enabled: !loading,
+//         queryFn: async () => {
+//             const res = await axiosSecure.get(`/savedusers/instructor/${user?.email}`);
+//             return res.data.instructor;
+//         }
+//     })
+//     return [isInstructor, isInstructorLoading]
+// }
+// export default useInstructor;
